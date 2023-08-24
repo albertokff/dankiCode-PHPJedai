fa27ef7870f0e347ed9bf1608e7710674d156fff
Diogo Alberto <kirchoffalberto@gmail.com>
2023-08-24 14:14:13 -0300

    Add Manipulando Arquivos e Pastas com PHP

---
+++ PHP/Manipulando Arquivos e Diretórios com PHP/Manipulando Pastas/index.php
@@ -0,0 +1,40 @@
+<?php
+	// mkdir('pasta');
+
+	/*
+		Código acima cria um diretório na pasta raiz do index.php
+	*/
+
+	// rmdir('pasta');
+
+	/*
+		Código acima remove um diretório na pasta raiz do index.php
+	*/
+
+
+	// if(is_dir('pasta')){
+	// 	echo 'é uma pasta válida';
+	// }
+
+	/*
+		Código acima verifica se existe um diretório chamado pasta na raiz desse index.php
+	*/
+
+	// if ($handle = opendir('pasta')) {
+
+	// 	while ($file = readdir($handle)) {
+	// 		if($file == '.' || $file == '..'){
+	// 			continue;
+	// 		}
+
+	// 		echo "$file\n";
+	// 		echo '<br />';
+	// 	}
+
+	// 	closedir($handle);
+	// }
+
+	/*
+		Código acima faz uma varredura na pasta e listas os arquivos e diretórios.
+	*/
+?>
